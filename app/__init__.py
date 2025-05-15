c1d30b275eb578754e24bcdbe46a418dcc1d5bdd
Santiago <santiagosepulveda322@gmail.com>
2025-05-15 02:14:47 -0500

    Fix gunicorn configuration

--- app/__init__.py
+++ app/__init__.py
@@ -1,12 +1,15 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
-from config import Config
 
 db = SQLAlchemy()
 
 def create_app():
     app = Flask(__name__)
-    app.config.from_object(Config)
+    
+    # Configuración
+    app.config['SECRET_KEY'] = 'dev-key-very-secret'
+    app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///robots_competencia.db'
+    app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
     
     db.init_app(app)
     
